9c01573d27675b4512a9a71bf81a29108c9500fc
michael19 <91liahim@gmail.com>
2021-04-06 19:02:26 +0500

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+
+export default {
+  head: {
+    title: 'nuxt-js-kit',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  css: [
+    './assets/styles/main.scss'
+  ],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: [
+    '@nuxtjs/dotenv',
+  ],
+
+  modules: [
+    '@nuxtjs/axios',
+  ],
+
+  axios: {},
+
+  build: {}
+}
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    Nuxt JS Kit
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
